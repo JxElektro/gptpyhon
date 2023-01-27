4460991a35cf13502714cbf82aa5e04926966ee6
Jhen Nuñez <jxelektro@gmail.com>
2023-01-26 21:11:46 -0300

    1 push

--- embedding.py
+++ embedding.py
@@ -2,24 +2,35 @@ import openai
 import numpy as np  # standard math module for python
 from pprint import pprint
 
+# function to open the text file where the API key is stored
+
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
+# function to get the embedding vector of a given content using the GPT-3 engine
+
 
 def gpt3_embedding(content, engine='text-similarity-babbage-001'):
-    content = content.encode(encoding='ASCII',errors='ignore').decode()
-    response = openai.Embedding.create(input=content,engine=engine)
+    # removing any unwanted characters
+    content = content.encode(encoding='ASCII', errors='ignore').decode()
+    # getting the embedding vector
+    response = openai.Embedding.create(input=content, engine=engine)
     vector = response['data'][0]['embedding']  # this is a normal list
     return vector
 
+# function to calculate the similarity of two vectors
 
-def similarity(v1, v2):  # return dot product of two vectors
+
+def similarity(v1, v2):
     return np.dot(v1, v2)
 
 
+# loading the API key
 openai.api_key = open_file('openaiapikey.txt')
+
+# function to match a given vector with a set of pre-defined categories
 
 
 def match_class(vector, classes):
@@ -32,17 +43,18 @@ def match_class(vector, classes):
 
 
 if __name__ == '__main__':
-    categories = ['plant', 'reptile', 'mammal', 'fish', 'bird', 'pet', 'wild animal']
+    # list of pre-defined categories
+    categories = ['plant', 'reptile', 'mammal',
+                  'fish', 'bird', 'pet', 'wild animal']
     classes = list()
+    # getting the embedding vector for each category
     for c in categories:
         vector = gpt3_embedding(c)
         info = {'category': c, 'vector': vector}
         classes.append(info)
-    #print(classes)
-    #exit(0)
+    # infinite loop to take input from user and match it with pre-defined categories
     while True:
         a = input('Enter a lifeform here: ')
         vector = gpt3_embedding(a)
-        #print(a, vector)
         result = match_class(vector, classes)
         pprint(result)
--- hello_world.py
+++ hello_world.py
@@ -1,17 +1,32 @@
-import openai
+import openai  # importing the openai library
 
 
-def open_file(filepath):
+def open_file(filepath):  # function to open the file containing the API key
+    # open the file with read permission, utf-8 encoding
     with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
+        return infile.read()  # return the content of the file
 
 
+# set the openai api key as the content of the file
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
-    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-    response = openai.Completion.create(
+
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['&#8203;`oaicite:{"index":0,"invalid_reason":"Malformed citation <<END>>"}`&#8203;']):
+    """
+    function to get the gpt3 completion
+    prompt : str : the prompt to give to the model
+    engine : str : the model to use, default text-davinci-002
+    temp : float : temperature for the model, default 0.7
+    top_p : float : top p for the model, default 1.0
+    tokens : int : maximum number of tokens to generate, default 400
+    freq_pen : float : frequency penalty, default 0.0
+    pres_pen : float : presence penalty, default 0.0
+    stop : list : list of stop tokens, default ["&#8203;`oaicite:{"index":1,"invalid_reason":"Malformed citation <<END>>"}`&#8203;"]
+    """
+    prompt = prompt.encode(encoding='ASCII', errors='ignore').decode(
+    )  # remove any non-ascii characters from the prompt
+    response = openai.Completion.create(  # send a request to the openai api
         engine=engine,
         prompt=prompt,
         temperature=temp,
@@ -20,11 +35,12 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
         stop=stop)
+    # extract the text from the response
     text = response['choices'][0]['text'].strip()
     return text
 
 
 if __name__ == '__main__':
     prompt = 'Write a list of famous American actors:'
-    response = gpt3_completion(prompt)
-    print(response)
+    response = gpt3_completion(prompt)  # get the response from the gpt3 model
+    print(response)  # print the response
